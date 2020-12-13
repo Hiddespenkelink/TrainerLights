e0addc268ae695d4e6bfd6fe829adab6453e5d77
Hiddespenkelink <hidde.spenkelink@gmail.com>
2020-12-13 19:08:54 +0100

    UltraSonic sensor toegevoegd en werkend

--- BlynkDevelop/BlinkdataSend/sketch_dec11a/sketch_dec11a.ino
+++ BlynkDevelop/BlinkdataSend/sketch_dec11a/sketch_dec11a.ino
@@ -4,6 +4,7 @@
         #include <ESP8266WiFi.h>                              // Enable the use of wifi module. Make sure you downloaded and installed the ESP8266 library
         #include <BlynkSimpleEsp8266.h>                       
         char auth[] = "pS6yoAOJtTu63e3ZPB_kTbZcNSO0d6sC";     // Put in the Auth Token for the project from Blynk. You should receive it in your email.
+        
                                                               // This Auth Token is for project A for Device A. If 2 devices using same project, then Auth Token can be same with Device B.
         char ssid[] = "Hidde";                  // Key in your wifi name. You can check with your smart phone for your wifi name
         char pass[] = "1234567890";                             // Key in your wifi password.
@@ -15,11 +16,20 @@
                                                               // Do not need to do Widget setting. It just let the Blynk server knows that you assign the pin for Bridge Widget.
         /* 1- Values on Device A */
 
-        int lampstatus;                                          // use Potentiometer as sensor
+        int lampState1;                                          // use Potentiometer as sensor
+        int lampState2;                                          // use Potentiometer as sensor
+        int lampState3;                                          // use Potentiometer as sensor
+        int lampState4;                                          // use Potentiometer as sensor
+
+
+        
         int countTime=0;                                      // use formula as data
         unsigned long startMillisReadData;                    // start counting time for value display
         int secondsDataDisplay = 1000;                        // use to calculate time for sensor. By default every 1000 milli seconds.
 
+        //
+
+        int raakState[8];
 
 BLYNK_CONNECTED() 
 { bridge1.setAuthToken("qhbu0KEVQeI611aHj0RTKW55-EWgX7yk"); } // Place Device B AuthToken here so that can access permission to write the values to Device B
@@ -32,11 +42,15 @@ void setup()
         Serial.begin(9600);                                   /* To assign communication port to communicate with meter. with 2 stop bits (refer to manual)*/
         Blynk.begin(auth, ssid, pass);                        /* Connecting to Blynk server */
         while (Blynk.connect() == false) {}                   // Wait until Blynk is connected
-        timer.setInterval(1000L, readValue);                  // Every 1000 milli seconds (1 second), run the Void readValue set of function                  
-        lampstatus = 10;
+        timer.setInterval(500L, readValue);                  // Every 1000 milli seconds (1 second), run the Void readValue set of function  
+
         /* 2 - Data submission to Blynk Server  */
 
         startMillisReadData = millis();                       /* Start counting time for data submission to Blynk Server*/
+
+        for (int i = 0; i < 8; i++){
+          raakState[i] = 0;
+        }
 }
 
 void loop() 
@@ -46,17 +60,41 @@ void loop()
         Blynk.run();                                                            /* keep the Blynk active */
         timer.run();                                                            /* keep the counter for Blynk active */
 
-        for (int i = 0; i < 100; i++){
-          lampstatus++;
+        if(raakState[1] == 0){
+          lampState = 1;
+        } else {
+          lampState = 0;
         }
-        
         
         /* 1 - Internal calculation in Device A  */
           
 }
 
+BLYNK_WRITE(V1)                                                     // Read the potentiometer values from Blynk Server
+{
+  raakState[1] = param.asInt();
+}
+
+BLYNK_WRITE(V2)                                                     // Read the potentiometer values from Blynk Server
+{
+  raakState[2] = param.asInt();
+}
+
+BLYNK_WRITE(V3)                                                     // Read the potentiometer values from Blynk Server
+{
+  raakState[3] = param.asInt();
+}
+
+BLYNK_WRITE(V4)                                                     // Read the potentiometer values from Blynk Server
+{
+  raakState[4] = param.asInt();
+}
+
 void readValue()                                                                /* it activates every second*/
 {
-        Blynk.virtualWrite(V1,lampstatus);                                         // send potRead to Device A (project A) web interface
-        bridge1.virtualWrite(V1,lampstatus);                                       // send potRead to Device B (project B) web interface 
+        bridge1.virtualWrite(V1,lampState1);                                       // send potRead to Device B (project B) web interface 
+//        bridge2.virtualWrite(V1,lampState2);                                       // send potRead to Device C (project C) web interface 
+//        bridge3.virtualWrite(V1,lampState3);                                       // send potRead to Device D (project D) web interface 
+//        bridge4.virtualWrite(V1,lampState4);                                       // send potRead to Device E (project E) web interface 
+                
 }
--- BlynkDevelop/BlinkdataSend/sketch_dec11b/sketch_dec11b.ino
+++ BlynkDevelop/BlinkdataSend/sketch_dec11b/sketch_dec11b.ino
@@ -1,4 +1,4 @@
-// Bridge Widget Device B using Blynk App By Solarduino 
+// Bridge Widget Device B using Blynk App By Solarduino
 
 // Note Summary
 // Note :  Safety is very important when dealing with electricity. We take no responsibilities while you do it at your own risk.
@@ -7,85 +7,100 @@
 // Note :  You need to create 2 Projects (with 2 Auth tokens from Blynk). Make sure to clearly identify which Auth Token is for which Device.
 // Note :  You need to download the Blynk App on your smart phone for display.
 // Note :  Solarduino only amend necessary code and integrate with Blynk Server.
-// Note :  Blog page for this code : 
+// Note :  Blog page for this code :
 
 /*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/////////////*/
 
-        
-        /* 0- Blynk Server and Wifi Connection */
 
-        #define BLYNK_PRINT Serial                            // to write data to the blynk server. Make sure you download and installed the Blynk library
-        #include <ESP8266WiFi.h>                              // Enable the use of wifi module. Make sure you downloaded and installed the ESP8266 library
-        #include <BlynkSimpleEsp8266.h>                       
-        char auth[] = "qhbu0KEVQeI611aHj0RTKW55-EWgX7yk";     // Put in the Auth Token for the project from Blynk. You should receive it in your email.
-                                                              // This is the Auth Token or project for Device B. If Device A & Device B sharing same project, can use same Atuth Token
-        char ssid[] = "Hidde";                  // Key in your wifi name. You can check with your smart phone for your wifi name
-        char pass[] = "1234567890";                             // Key in your wifi password.
-        BlynkTimer timer;                                     // Create variable for timer function in Blynk App.
-        
-        /* 0.1 - Bridge Widget Blynk Server */
-        
-        WidgetBridge bridge1(V10);                            // Assign virtual pin 10 of this device (Device A) for Bridge Widget to communicate with other Devices (Device B) and the channel is named "bridge1"
-                                                              // Do not need to do Widget setting. It just let the Blynk server knows that you assign the pin for Bridge Widget. 
-        
-        /* 1- Values on Device B */
+/* 0- Blynk Server and Wifi Connection */
 
-        int potRead=0;                                        // create variable for potRead values received from Device A
-        int countTime=0;                                      // create variable for countTime values received from Device A
-        int combine=0;                                        // create variable for potRead x countTime values
-        unsigned long startMillisReadData;                    // start counting time for value display
-        int secondsDataDisplay = 1000;                        // use to calculate time for sensor. By default every 1000 milli seconds.
+#define BLYNK_PRINT Serial                            // to write data to the blynk server. Make sure you download and installed the Blynk library
+#define echoPin D2 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin D1 //attach pin D3 Arduino to pin Trig of HC-SR04
 
-void setup() 
+
+#include <ESP8266WiFi.h>                              // Enable the use of wifi module. Make sure you downloaded and installed the ESP8266 library
+#include <BlynkSimpleEsp8266.h>
+char auth[] = "qhbu0KEVQeI611aHj0RTKW55-EWgX7yk";     // Put in the Auth Token for the project from Blynk. You should receive it in your email.
+// This is the Auth Token or project for Device B. If Device A & Device B sharing same project, can use same Atuth Token
+char ssid[] = "Hidde";                  // Key in your wifi name. You can check with your smart phone for your wifi name
+char pass[] = "1234567890";                             // Key in your wifi password.
+BlynkTimer timer;                                     // Create variable for timer function in Blynk App.
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+
+
+
+/* 0.1 - Bridge Widget Blynk Server */
+
+WidgetBridge bridge0(V10);                            // Assign virtual pin 10 of this device (Device B) for Bridge Widget to communicate with other Devices (Device A) and the channel is named "bridge0"
+// Do not need to do Widget setting. It just let the Blynk server knows that you assign the pin for Bridge Widget.
+
+/* 1- Values on Device B */
+int raakState = 0;
+int lampState = 0;
+int potRead = 0;                                      // create variable for potRead values received from Device A
+int countTime = 0;                                    // create variable for countTime values received from Device A
+unsigned long startMillisReadData;                    // start counting time for value display
+int secondsDataDisplay = 1000;                        // use to calculate time for sensor. By default every 1000 milli seconds.
+
+BLYNK_CONNECTED() 
+{ bridge0.setAuthToken("pS6yoAOJtTu63e3ZPB_kTbZcNSO0d6sC"); }
+
+
+void setup()
 {
-       
-        /*0 General*/
 
-        Serial.begin(9600);                                   /* To assign communication with Serial Monitor*/
-        Blynk.begin(auth, ssid, pass);                        /* Connecting to Blynk server */
-        while (Blynk.connect() == false) {}                   // Wait until Blynk is connected
-        timer.setInterval(1000L, readValue);                  // Every 1000 milli seconds (1 second), run the Void readValue set of function 
-                                            
-        /* 2 - Data submission to Blynk Server  */
+  /*0 General*/
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  Serial.begin(9600);                                   /* To assign communication with Serial Monitor*/
+  Blynk.begin(auth, ssid, pass);                        /* Connecting to Blynk server */
+  while (Blynk.connect() == false) {}                   // Wait until Blynk is connected
+  timer.setInterval(500L, readValue);                  // Every 1000 milli seconds (1 second), run the Void readValue set of function
+  pinMode(D6, OUTPUT);
+  /* 2 - Data submission to Blynk Server  */
 
-        startMillisReadData = millis();                       /* Start counting time for data submission to Blynk Server*/
+  startMillisReadData = millis();                       /* Start counting time for data submission to Blynk Server*/
 }
 
-void loop() 
+void loop()
 {
-        /* 0- General */
-        
-        Blynk.run();                                                /* keep the Blynk active */
-        timer.run();                                                /* keep the counter for Blynk active */
-        
-        /* 1 - Internal calculation in Device B  */
-        
-        if(millis() - startMillisReadData >= secondsDataDisplay)    /* every second, run the code*/
-        {
-        Serial.print("potRead : ");
-        Serial.println(potRead);                                    /* Read the potentiometer in analog values (0-1024)*/
-        Serial.print("countTime : ");
-        Serial.println(countTime);                                  /* Read the time in seconds*/
-        combine = potRead*countTime;    
-        Serial.print("combine : ");
-        Serial.println(combine);
-        startMillisReadData = millis(); 
-        }    
+  /* 0- General */
+
+  Blynk.run();                                                /* keep the Blynk active */
+  timer.run();                                                /* keep the counter for Blynk active */
+  if (lampState == 1) {
+    digitalWrite(D6, HIGH);
+  } else {
+    digitalWrite(D6, LOW);
+  }
+
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+
+  if (distance < 10){
+    raakState =1;
+  }
 }
 
 BLYNK_WRITE(V1)                                                     // Read the potentiometer values from Blynk Server
 {
-potRead = param.asInt();
-Serial.println(potRead);
-}
-
-BLYNK_WRITE(V2)                                                     // Read the countTime values from Blynk Server
-{
-countTime = param.asInt();
-Serial.println(countTime);
+  lampState = param.asInt();
 }
 
 void readValue()                                                    // it activates every second
 {
-Blynk.virtualWrite(V3,combine);                                     // Re-post the Combined Values up to the Blynk App
+  bridge0.virtualWrite(V1,raakState);                                       // send potRead to Device A (project A) web interface 
+
 }
