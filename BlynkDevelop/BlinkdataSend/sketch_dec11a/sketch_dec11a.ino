7f529b4f6ea2912b3b70705cca6bef609300bc67
Hiddespenkelink <hidde.spenkelink@gmail.com>
2021-02-02 19:26:47 +0100

    Trainerlights v1.0
    Werkende Trainerlights (Voor nu alleen blauw licht)

--- BlynkDevelop/BlinkdataSend/sketch_dec11a/sketch_dec11a.ino
+++ BlynkDevelop/BlinkdataSend/sketch_dec11a/sketch_dec11a.ino
@@ -1,47 +1,51 @@
 /* 0- Blynk Server and Wifi Connection */
 
-#define BLYNK_PRINT Serial                            // to write data to the blynk server. Make sure you download and installed the Blynk library
-#define echoPin D2 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin D1 //attach pin D3 Arduino to pin Trig of HC-SR04
+#define BLYNK_PRINT Serial // to write data to the blynk server. Make sure you download and installed the Blynk library
+#define echoPin D2         // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin D1         //attach pin D3 Arduino to pin Trig of HC-SR04
 
-#include <ESP8266WiFi.h>                              // Enable the use of wifi module. Make sure you downloaded and installed the ESP8266 library
+#include <ESP8266WiFi.h> // Enable the use of wifi module. Make sure you downloaded and installed the ESP8266 library
 #include <BlynkSimpleEsp8266.h>
-char auth[] = "pS6yoAOJtTu63e3ZPB_kTbZcNSO0d6sC";     // Put in the Auth Token for the project from Blynk. You should receive it in your email.
+char auth[] = "pS6yoAOJtTu63e3ZPB_kTbZcNSO0d6sC"; // Put in the Auth Token for the project from Blynk. You should receive it in your email.
 
 // This Auth Token is for project A for Device A. If 2 devices using same project, then Auth Token can be same with Device B.
-char ssid[] = "Hidde";                  // Key in your wifi name. You can check with your smart phone for your wifi name
-char pass[] = "1234567890";                             // Key in your wifi password.
-BlynkTimer timer;                                     // Create variable for Timer function in Blynk App.
+char ssid[] = "Hidde";      // Key in your wifi name. You can check with your smart phone for your wifi name
+char pass[] = "1234567890"; // Key in your wifi password.
+BlynkTimer timer;           // Create variable for Timer function in Blynk App.
 
 /* 0.1 - Bridge Widget Blynk Server */
 
-WidgetBridge bridge1(V11);                            // Assign virtual pin 10 of this device (Device A) for Bridge Widget to communicate with other Devices (Device B) and the channel is named "bridge1"
-WidgetBridge bridge2(V12);                            // Assign virtual pin 10 of this device (Device A) for Bridge Widget to communicate with other Devices (Device B) and the channel is named "bridge1"
-WidgetBridge bridge3(V13);                            // Assign virtual pin 10 of this device (Device A) for Bridge Widget to communicate with other Devices (Device B) and the channel is named "bridge1"
+WidgetBridge bridge1(V11); // Assign virtual pin 10 of this device (Device A) for Bridge Widget to communicate with other Devices (Device B) and the channel is named "bridge1"
+WidgetBridge bridge2(V12); // Assign virtual pin 10 of this device (Device A) for Bridge Widget to communicate with other Devices (Device B) and the channel is named "bridge1"
+WidgetBridge bridge3(V13); // Assign virtual pin 10 of this device (Device A) for Bridge Widget to communicate with other Devices (Device B) and the channel is named "bridge1"
 // Do not need to do Widget setting. It just let the Blynk server knows that you assign the pin for Bridge Widget.
 /* 1- Values on Device A */
+
+int startState = 0;
+int aantalInterval = 10;
+
 int lampState[4] = {0, 0, 0, 0};
-int lampState1;                                          // use Potentiometer as sensor
-int lampState2;                                          // use Potentiometer as sensor
-int lampState3;                                          // use Potentiometer as sensor
-int lampState4;                                          // use Potentiometer as sensor
+int lampState1; // use Potentiometer as sensor
+int lampState2; // use Potentiometer as sensor
+int lampState3; // use Potentiometer as sensor
+int lampState4; // use Potentiometer as sensor
 int selectionLamp;
 int selectionMade = 0;
 
 long duration; // variable for the duration of sound wave travel
-int distance; // variable for the distance measurement
+int distance;  // variable for the distance measurement
 double timeSet;
 double reactieTijd;
 double bufferTijd;
 
+double Reactietijd[255];
+double gemReactietijd = 0;
 
-double gemReactietijd;
+double rt[4];
 
-double rt = 0 ;
-
-int countTime = 0;                                    // use formula as data
-unsigned long startMillisReadData;                    // start counting time for value display
-int secondsDataDisplay = 1000;                        // use to calculate time for sensor. By default every 1000 milli seconds.
+int countTime = 0;                 // use formula as data
+unsigned long startMillisReadData; // start counting time for value display
+int secondsDataDisplay = 1000;     // use to calculate time for sensor. By default every 1000 milli seconds.
 
 //
 
@@ -49,82 +53,139 @@ int raakState[8];
 
 BLYNK_CONNECTED()
 {
-  bridge1.setAuthToken("qhbu0KEVQeI611aHj0RTKW55-EWgX7yk");  // Place Device B AuthToken here so that can access permission to write the values to Device B
-  bridge2.setAuthToken("DBO3UcGnFjdiIiH2pjtb0nQa2FXI8xFI");  // Place Device C AuthToken here so that can access permission to write the values to Device B
-  bridge3.setAuthToken("pwSYUIQdVySoCVd5eJrFQMgr6iaUQdBZ");  // Place Device D AuthToken here so that can access permission to write the values to Device B
+  bridge1.setAuthToken("qhbu0KEVQeI611aHj0RTKW55-EWgX7yk"); // Place Device B AuthToken here so that can access permission to write the values to Device B
+  bridge2.setAuthToken("DBO3UcGnFjdiIiH2pjtb0nQa2FXI8xFI"); // Place Device C AuthToken here so that can access permission to write the values to Device B
+  bridge3.setAuthToken("pwSYUIQdVySoCVd5eJrFQMgr6iaUQdBZ"); // Place Device D AuthToken here so that can access permission to write the values to Device B
 }
 
-void setup_init() {
+void setup_init()
+{
   /*0 General*/
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
-  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an INPUT
   pinMode(D6, OUTPUT);
-  Serial.begin(9600);                                   /* To assign communication port to communicate with meter. with 2 stop bits (refer to manual)*/
-  Blynk.begin(auth, ssid, pass);                        /* Connecting to Blynk server */
-  while (Blynk.connect() == false) {}                   // Wait until Blynk is connected
-  timer.setInterval(500L, readValue);                  // Every 1000 milli seconds (1 second), run the Void readValue set of function
+  Serial.begin(9600);            /* To assign communication port to communicate with meter. with 2 stop bits (refer to manual)*/
+  Blynk.begin(auth, ssid, pass); /* Connecting to Blynk server */
+  while (Blynk.connect() == false)
+  {
+  }                                   // Wait until Blynk is connected
+  timer.setInterval(500L, readValue); // Every 1000 milli seconds (1 second), run the Void readValue set of function
 
   /* 2 - Data submission to Blynk Server  */
-  startMillisReadData = millis();                       /* Start counting time for data submission to Blynk Server*/
+  startMillisReadData = millis(); /* Start counting time for data submission to Blynk Server*/
 
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 8; i++)
+  {
     raakState[i] = 0;
+  }
+
+  for (int i = 0; i < 255; i++)
+  {
+    Reactietijd[0] = 0;
+  }
+
+  for (int i = 0; i < 4; i++)
+  {
+    rt[i] = 0;
   }
 }
 
-void main_loop() {
-  Blynk.run();                                                            /* keep the Blynk active */
-  timer.run();                                                            /* keep the counter for Blynk active */
+void main_loop()
+{
+  Blynk.run(); /* keep the Blynk active */
+  timer.run(); /* keep the counter for Blynk active */
+  int counter = 0;
+  if (startState)
+  {
+    digitalWrite(D6, LOW);
+    gemReactietijd = 0;
+    for (int i = 0; i < 3; i++)
+    {
+      lampState[i + 1] = 0;
+    }
+    while (startState && aantalInterval >= counter)
+    {
+      Blynk.run(); /* keep the Blynk active */
+      timer.run(); /* keep the counter for Blynk active */
+      if (selectionMade == 0)
+      {
+        selectionLamp = rand() % 4;
+        selectionMade = 1;
+        Serial.print("dit is: ");
+        Serial.println(selectionLamp);
+        counter++;
+      }
 
-  if (selectionMade == 0) {
-    selectionLamp = rand() % 4;
-    selectionMade = 1;
-    Serial.print("dit is: "); Serial.println(selectionLamp);
+      if (raakState[selectionLamp] == 0)
+      { //niet aangeraakt dan lamp aan, anders lamp uit en nieuwe selectie
+        lampState[selectionLamp] = 1;
+      }
+      else
+      {
+        Reactietijd[counter] = rt[selectionLamp];
+        lampState[selectionLamp] = 0;
+        raakState[selectionLamp] = 0;
+        selectionMade = 0;
+      }
 
+      if (selectionLamp == 0)
+      {
+        if (lampState[0] == 1)
+        {
+          digitalWrite(D6, HIGH);
+          if (timeSet == 0)
+          {
+            bufferTijd = millis();
+            timeSet = 1;
+          }
+        }
+        else
+        {
+          digitalWrite(D6, LOW);
+          timeSet = 0;
+        }
+
+        // Clears the trigPin condition
+        digitalWrite(trigPin, LOW);
+        delayMicroseconds(2);
+        // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+        digitalWrite(trigPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trigPin, LOW);
+        // Reads the echoPin, returns the sound wave travel time in microseconds
+        duration = pulseIn(echoPin, HIGH);
+        // Calculating the distance
+        distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+        // Displays the distance on the Serial Monitor
+        //  Serial.println(distance);
+
+        if (distance < 10 && selectionLamp == 0)
+        {
+          raakState[0] = 1;
+          digitalWrite(D6, LOW);
+          if (timeSet == 1)
+          {
+            rt[0] = (millis() - bufferTijd) / 1000;
+          }
+        }
+      }
+    }
+    startState = 0;
+    //bereken gemReactietijd
+    for (int i = 0; i < counter; i++)
+    {
+      gemReactietijd = gemReactietijd + Reactietijd[i];
+    }
+    gemReactietijd = gemReactietijd / counter;
   }
-
-  if (raakState[selectionLamp] == 0) {                                  //niet aangeraakt dan lamp aan, anders lamp uit en nieuwe selectie
-    lampState[selectionLamp] = 1;
-  } else {
-    lampState[selectionLamp] = 0;
-    raakState[selectionLamp] = 0;
-    selectionMade = 0;
-    
-  }
-
-  if (lampState[0] == 1) {
+  else
+  {
     digitalWrite(D6, HIGH);
-    if (timeSet == 0) {
-      bufferTijd = millis();
-      timeSet = 1;
-    }
-  } else {
-    digitalWrite(D6, LOW);
-    timeSet = 0;
-  }
-
-  // Clears the trigPin condition
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  // Displays the distance on the Serial Monitor
-//  Serial.println(distance);
-
-  if (distance < 10 && selectionLamp == 0) {
-    raakState[0] = 1;
-    digitalWrite(D6, LOW);
-    if (timeSet == 1) {
-      reactieTijd = (millis() - bufferTijd) / 1000;
+    for (int i = 0; i < 3; i++)
+    {
+      lampState[i + 1] = 1;
     }
   }
-  gemReactietijd = (gemReactietijd + rt) / 2;
 }
 
 void setup()
@@ -137,41 +198,53 @@ void loop()
   main_loop();
 }
 
-BLYNK_WRITE(V1)                                                     // Read the potentiometer values from Blynk Server
+BLYNK_WRITE(V1) // Read the raak values from Blynk Server
 {
   raakState[1] = param.asInt();
 }
 
-BLYNK_WRITE(V2)                                                     // Read the potentiometer values from Blynk Server
+BLYNK_WRITE(V2) // Read the raak values from Blynk Server
 {
   raakState[2] = param.asInt();
 }
 
-BLYNK_WRITE(V3)                                                     // Read the potentiometer values from Blynk Server
+BLYNK_WRITE(V3) // Read the raak values from Blynk Server
 {
   raakState[3] = param.asInt();
 }
 
-BLYNK_WRITE(V91)                                                     // Read the potentiometer values from Blynk Server
+BLYNK_WRITE(V91) // Read the reactietijd values from Blynk Server
 {
-  rt = param.asDouble();
+  rt[1] = param.asDouble();
 }
 
-BLYNK_WRITE(V92)                                                     // Read the potentiometer values from Blynk Server
+BLYNK_WRITE(V92) // Read the reactietijd values from Blynk Server
 {
-  rt = param.asDouble();
+  rt[2] = param.asDouble();
 }
 
-BLYNK_WRITE(V93)                                                     // Read the potentiometer values from Blynk Server
+BLYNK_WRITE(V93) // Read the reactietijd values from Blynk Server
 {
-  rt = param.asDouble();
+  rt[3] = param.asDouble();
 }
 
-void readValue()                                                                /* it activates every second*/
+BLYNK_WRITE(V25) // Read the reactietijd values from Blynk Server
 {
-  Blynk.virtualWrite(V120, gemReactietijd);                                      // send potRead to Device B (project B) web interface
-  bridge1.virtualWrite(V1, lampState[1]);                                      // send potRead to Device B (project B) web interface
-  bridge2.virtualWrite(V1, lampState[2]);                                      // send potRead to Device C (project C) web interface
-  bridge3.virtualWrite(V1, lampState[3]);                                      // send potRead to Device D (project D) web interface
+  startState = param.asInt();
+}
 
+BLYNK_WRITE(V20) // Read the reactietijd values from Blynk Server
+{
+  aantalInterval = param.asInt();
+}
+
+void readValue() /* it activates every second*/
+{
+  Blynk.virtualWrite(V120, gemReactietijd); // send potRead to Device B (project B) web interface
+  Blynk.virtualWrite(V90, reactieTijd);
+  bridge1.virtualWrite(V1, lampState[1]); // send potRead to Device B (project B) web interface
+  bridge2.virtualWrite(V1, lampState[2]); // send potRead to Device C (project C) web interface
+  bridge3.virtualWrite(V1, lampState[3]); // send potRead to Device D (project D) web interface
+  Blynk.virtualWrite(V25, startState);
+  Blynk.virtualWrite(V20, aantalInterval);
 }
--- BlynkDevelop/BlinkdataSend/sketch_dec11d/sketch_dec11d.ino
+++ BlynkDevelop/BlinkdataSend/sketch_dec11d/sketch_dec11d.ino
@@ -9,113 +9,131 @@
 // Note :  Solarduino only amend necessary code and integrate with Blynk Server.
 // Note :  Blog page for this code :
 
-/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/////////////*/
-
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/ ////////////*/
 
 /* 0- Blynk Server and Wifi Connection */
 
-#define BLYNK_PRINT Serial                            // to write data to the blynk server. Make sure you download and installed the Blynk library
-#define echoPin D2 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin D1 //attach pin D3 Arduino to pin Trig of HC-SR04
+#define BLYNK_PRINT Serial // to write data to the blynk server. Make sure you download and installed the Blynk library
+#define echoPin D2         // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin D1         //attach pin D3 Arduino to pin Trig of HC-SR04
 
-
-#include <ESP8266WiFi.h>                              // Enable the use of wifi module. Make sure you downloaded and installed the ESP8266 library
+#include <ESP8266WiFi.h> // Enable the use of wifi module. Make sure you downloaded and installed the ESP8266 library
 #include <BlynkSimpleEsp8266.h>
-char auth[] = "pwSYUIQdVySoCVd5eJrFQMgr6iaUQdBZ";     // Put in the Auth Token for the project from Blynk. You should receive it in your email.
+char auth[] = "pwSYUIQdVySoCVd5eJrFQMgr6iaUQdBZ"; // Put in the Auth Token for the project from Blynk. You should receive it in your email.
 // This is the Auth Token or project for Device B. If Device A & Device B sharing same project, can use same Atuth Token
-char ssid[] = "Hidde";                  // Key in your wifi name. You can check with your smart phone for your wifi name
-char pass[] = "1234567890";                             // Key in your wifi password.
-BlynkTimer timer;                                     // Create variable for timer function in Blynk App.
-long duration; // variable for the duration of sound wave travel
-int distance; // variable for the distance measurement
-
-
+char ssid[] = "Hidde";      // Key in your wifi name. You can check with your smart phone for your wifi name
+char pass[] = "1234567890"; // Key in your wifi password.
+BlynkTimer timer;           // Create variable for timer function in Blynk App.
+long duration;              // variable for the duration of sound wave travel
+int distance;               // variable for the distance measurement
 
 /* 0.1 - Bridge Widget Blynk Server */
 
-WidgetBridge bridge0(V10);                            // Assign virtual pin 10 of this device (Device B) for Bridge Widget to communicate with other Devices (Device A) and the channel is named "bridge0"
+WidgetBridge bridge0(V10); // Assign virtual pin 10 of this device (Device B) for Bridge Widget to communicate with other Devices (Device A) and the channel is named "bridge0"
 // Do not need to do Widget setting. It just let the Blynk server knows that you assign the pin for Bridge Widget.
 
 /* 1- Values on Device B */
 int raakState = 0;
 int lampState = 0;
-int potRead = 0;                                      // create variable for potRead values received from Device A
-int countTime = 0;                                    // create variable for countTime values received from Device A
-unsigned long startMillisReadData;                    // start counting time for value display
-int secondsDataDisplay = 1000;                        // use to calculate time for sensor. By default every 1000 milli seconds.
+int potRead = 0;                   // create variable for potRead values received from Device A
+int countTime = 0;                 // create variable for countTime values received from Device A
+unsigned long startMillisReadData; // start counting time for value display
+int secondsDataDisplay = 1000;     // use to calculate time for sensor. By default every 1000 milli seconds.
+
+int skipcounter = 100;
 
 double reactieTijd;
 double bufferTijd;
 double timeSet;
 
-BLYNK_CONNECTED() 
-{ bridge0.setAuthToken("pS6yoAOJtTu63e3ZPB_kTbZcNSO0d6sC"); }
-
+BLYNK_CONNECTED()
+{
+  bridge0.setAuthToken("pS6yoAOJtTu63e3ZPB_kTbZcNSO0d6sC");
+}
 
 void setup()
 {
   /*0 General*/
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
-  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
-  Serial.begin(9600);                                   /* To assign communication with Serial Monitor*/
-  Blynk.begin(auth, ssid, pass);                        /* Connecting to Blynk server */
-  while (Blynk.connect() == false) {}                   // Wait until Blynk is connected
-  timer.setInterval(500L, readValue);                  // Every 1000 milli seconds (1 second), run the Void readValue set of function
+  pinMode(trigPin, OUTPUT);      // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT);       // Sets the echoPin as an INPUT
+  Serial.begin(9600);            /* To assign communication with Serial Monitor*/
+  Blynk.begin(auth, ssid, pass); /* Connecting to Blynk server */
+  while (Blynk.connect() == false)
+  {
+  }                                   // Wait until Blynk is connected
+  timer.setInterval(500L, readValue); // Every 1000 milli seconds (1 second), run the Void readValue set of function
   pinMode(D6, OUTPUT);
   /* 2 - Data submission to Blynk Server  */
 
-  startMillisReadData = millis();                       /* Start counting time for data submission to Blynk Server*/
+  startMillisReadData = millis(); /* Start counting time for data submission to Blynk Server*/
 }
 
 void loop()
 {
   /* 0- General */
 
-  Blynk.run();                                                /* keep the Blynk active */
-  timer.run(); 
-  
-  /* keep the counter for Blynk active */
-  if (lampState == 1) {
-    digitalWrite(D6, HIGH);
-    if (timeSet == 0){
-    bufferTijd = millis();
-    timeSet = 1;
+  Blynk.run(); /* keep the Blynk active */
+  timer.run();
+
+  if (skipcounter >= 100)
+  {
+    /* keep the counter for Blynk active */
+    if (lampState == 1)
+    {
+      digitalWrite(D6, HIGH);
+      if (timeSet == 0)
+      {
+        bufferTijd = millis();
+        timeSet = 1;
+      }
     }
-  } else {
-    digitalWrite(D6, LOW);
-    timeSet = 0;
-    raakState = 0;
+    else
+    {
+      digitalWrite(D6, LOW);
+      timeSet = 0;
+      raakState = 0;
+      delay(2000);
+    }
+
+    // Clears the trigPin condition
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    // Calculating the distance
+    distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+    // Displays the distance on the Serial Monitor
+
+    if (distance < 10)
+    {
+      raakState = 1;
+      digitalWrite(D6, LOW);
+      if (timeSet == 1)
+      {
+        reactieTijd = (millis() - bufferTijd) / 1000;
+      }
+      //skip lichtactiviteiten en puls voor een tijdje
+      skipcounter = 0;
+    }
   }
-
-  // Clears the trigPin condition
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  // Displays the distance on the Serial Monitor
-
-  if (distance < 10){
-    raakState = 1;
-    digitalWrite(D6, LOW);
-    if (timeSet == 1){
-    reactieTijd = (millis() - bufferTijd) / 1000;
-    }
+  else
+  {
+    skipcounter++;
+    delay(10);
   }
 }
 
-BLYNK_WRITE(V1)                                                     // Read the potentiometer values from Blynk Server
+BLYNK_WRITE(V1) // Read the potentiometer values from Blynk Server
 {
   lampState = param.asInt();
 }
 
-void readValue()                                                    // it activates every second
+void readValue() // it activates every second
 {
-  bridge0.virtualWrite(V3,raakState);                                       // send potRead to Device A (project A) web interface 
-  bridge0.virtualWrite(V93,reactieTijd); 
+  bridge0.virtualWrite(V3, raakState); // send potRead to Device A (project A) web interface
+  bridge0.virtualWrite(V93, reactieTijd);
 }
